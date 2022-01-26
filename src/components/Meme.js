60d39edf7270c77f28fad6a1a157ff8556f6806c
mattia-giordano <m.giordano019@gmail.com>
2022-01-27 00:39:38 +0100

    first commit

---
+++ src/components/Meme.js
@@ -0,0 +1,12 @@
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input type="text" placeholder="top text" className="form--input"/>
+                <input type="text" placeholder="bottom text" className="form--input"/>
+                <button className="form--button">Get a new meme image 🖼</button>
+            </form>
+        </main>
+    )
+}
